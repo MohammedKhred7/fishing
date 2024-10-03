4db943e25c035d2b98b5b845fbe6ef6066a13532
MohammedKhred7 <mhmdkhrd9@gmail.com>
2024-10-03 21:30:58 +0300

    fix: some bug

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // github/
+    base:"/fishing/",
+
   plugins: [
     vue(),
     vuetify(),
