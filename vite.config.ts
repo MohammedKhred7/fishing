7ea71fb54d1e032fca0b7055476b9f8d209cf888
MohammedKhred7 <mhmdkhrd9@gmail.com>
2024-10-04 10:37:31 +0300

    deploy all

--- vite.config.ts
+++ vite.config.ts
@@ -8,16 +8,13 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   // github/
-    // base:"/fishing/",
+    base:"/fishing/",
 
   plugins: [
     vue(),
     vuetify(),
     vueJsx()
   ],
-  // define: {
-  //   'process.env': {}
-  // },
   css: {
     preprocessorOptions: {
       scss: {
