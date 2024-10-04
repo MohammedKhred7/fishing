ca4384da46d5e946374c59344bb0c3bed5bdf943
MohammedKhred7 <mhmdkhrd9@gmail.com>
2024-10-04 10:49:09 +0300

    deploy2

--- dist/assets/notFound-59b415f0.js
+++
@@ -1,1 +0,0 @@
-import{aT as o,K as e,L as t}from"./index-8c8de8dd.js";const n={};function a(c,r,s,p,_,u){return e(),t("h2",null,"404 Page Not Found")}const f=o(n,[["render",a]]);export{f as default};
---
+++ dist/assets/notFound-d7e80355.js
@@ -0,0 +1,1 @@
+import{aT as o,K as e,L as t}from"./index-3f0681b6.js";const n={};function a(c,r,s,p,_,u){return e(),t("h2",null,"404 Page Not Found")}const f=o(n,[["render",a]]);export{f as default};
